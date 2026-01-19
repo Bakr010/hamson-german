7d8a703c550ec864692ae0368796b1f6f7a0247b
Radhi Bajahaw <rathi7137@gmail.com>
2026-01-19 21:24:00 +0100

    prepare for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/hamson-german/',
   plugins: [
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
-    assetsInlineLimit: 100000000,
-  },
 })
